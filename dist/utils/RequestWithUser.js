38c56b268a7cb198745bf81aaf9b90f9826062a2
Eyohen <40663724+Eyohen@users.noreply.github.com>
2024-06-13 18:50:28 +0100

    Delete dist directory

--- dist/utils/RequestWithUser.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const models_1 = __importDefault(require("../models"));
-const { User } = models_1.default; // Import the User model or adjust the import as needed
